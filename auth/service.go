b3e4372fcb72fdfa35ac6664feeb8cf3aa023425
felix.gateru <felix.gateru@gmail.com>
2023-11-22 20:23:40 +0300

    Rename import aliases
    Signed-off-by: felix.gateru <felix.gateru@gmail.com>

--- auth/service.go
+++ auth/service.go
@@ -195,7 +195,7 @@ func (svc service) Authorize(ctx context.Context, pr PolicyReq) error {
 
 func (svc service) AddPolicy(ctx context.Context, pr PolicyReq) error {
 	if err := svc.PolicyValidation(pr); err != nil {
-		return errors.Wrap(errors.ErrNotFound, err)
+		return errors.Wrap(svcerr.ErrNotFound, err)
 	}
 	return svc.agent.AddPolicy(ctx, pr)
 }
@@ -210,7 +210,7 @@ func (svc service) PolicyValidation(pr PolicyReq) error {
 func (svc service) AddPolicies(ctx context.Context, prs []PolicyReq) error {
 	for _, pr := range prs {
 		if err := svc.PolicyValidation(pr); err != nil {
-			return errors.Wrap(errors.ErrNotFound, err)
+			return errors.Wrap(svcerr.ErrNotFound, err)
 		}
 	}
 	return svc.agent.AddPolicies(ctx, prs)
@@ -223,7 +223,7 @@ func (svc service) DeletePolicy(ctx context.Context, pr PolicyReq) error {
 func (svc service) DeletePolicies(ctx context.Context, prs []PolicyReq) error {
 	for _, pr := range prs {
 		if err := svc.PolicyValidation(pr); err != nil {
-			return errors.Wrap(errors.ErrNotFound, err)
+			return errors.Wrap(svcerr.ErrNotFound, err)
 		}
 	}
 	return svc.agent.DeletePolicies(ctx, prs)
@@ -235,7 +235,7 @@ func (svc service) ListObjects(ctx context.Context, pr PolicyReq, nextPageToken 
 	}
 	res, npt, err := svc.agent.RetrieveObjects(ctx, pr, nextPageToken, limit)
 	if err != nil {
-		return PolicyPage{}, errors.Wrap(errors.ErrNotFound, err)
+		return PolicyPage{}, errors.Wrap(svcerr.ErrNotFound, err)
 	}
 	var page PolicyPage
 	for _, tuple := range res {
@@ -248,7 +248,7 @@ func (svc service) ListObjects(ctx context.Context, pr PolicyReq, nextPageToken 
 func (svc service) ListAllObjects(ctx context.Context, pr PolicyReq) (PolicyPage, error) {
 	res, err := svc.agent.RetrieveAllObjects(ctx, pr)
 	if err != nil {
-		return PolicyPage{}, errors.Wrap(errors.ErrNotFound, err)
+		return PolicyPage{}, errors.Wrap(svcerr.ErrNotFound, err)
 	}
 	var page PolicyPage
 	for _, tuple := range res {
@@ -267,26 +267,26 @@ func (svc service) ListSubjects(ctx context.Context, pr PolicyReq, nextPageToken
 	}
 	res, npt, err := svc.agent.RetrieveSubjects(ctx, pr, nextPageToken, limit)
 	if err != nil {
-		return PolicyPage{}, errors.Wrap(errors.ErrNotFound, err)
+		return PolicyPage{}, errors.Wrap(svcerr.ErrNotFound, err)
 	}
 	var page PolicyPage
 	for _, tuple := range res {
 		page.Policies = append(page.Policies, tuple.Subject)
 	}
 	page.NextPageToken = npt
-	return page, errors.Wrap(errors.ErrNotFound, err)
+	return page, errors.Wrap(svcerr.ErrNotFound, err)
 }
 
 func (svc service) ListAllSubjects(ctx context.Context, pr PolicyReq) (PolicyPage, error) {
 	res, err := svc.agent.RetrieveAllSubjects(ctx, pr)
 	if err != nil {
-		return PolicyPage{}, errors.Wrap(errors.ErrNotFound, err)
+		return PolicyPage{}, errors.Wrap(svcerr.ErrNotFound, err)
 	}
 	var page PolicyPage
 	for _, tuple := range res {
 		page.Policies = append(page.Policies, tuple.Subject)
 	}
-	return page, errors.Wrap(errors.ErrNotFound, err)
+	return page, errors.Wrap(svcerr.ErrNotFound, err)
 }
 
 func (svc service) CountSubjects(ctx context.Context, pr PolicyReq) (int, error) {
