87b2b1460444edb930f146cfa638996c1f8045a1
felix.gateru <felix.gateru@gmail.com>
2023-11-23 07:34:01 +0300

    Update auth service
    Signed-off-by: felix.gateru <felix.gateru@gmail.com>

--- auth/api/http/domains/transport.go
+++ auth/api/http/domains/transport.go
@@ -8,82 +8,79 @@ import (
 	"github.com/absmach/magistrala/internal/api"
 	"github.com/absmach/magistrala/internal/apiutil"
 	"github.com/absmach/magistrala/logger"
+	"github.com/go-chi/chi"
 	kithttp "github.com/go-kit/kit/transport/http"
-	"github.com/go-zoo/bone"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
-func MakeHandler(svc auth.Service, r *bone.Mux, logger logger.Logger) *bone.Mux {
+func MakeHandler(svc auth.Service, mux *chi.Mux, logger logger.Logger) *chi.Mux {
 	opts := []kithttp.ServerOption{
 		kithttp.ServerErrorEncoder(apiutil.LoggingErrorEncoder(logger, api.EncodeError)),
 	}
 
-	dr := bone.New()
+	mux.Route("/domains", func(r chi.Router) {
+		r.Post("/", otelhttp.NewHandler(kithttp.NewServer(
+			createDomainEndpoint(svc),
+			decodeCreateDomainRequest,
+			api.EncodeResponse,
+			opts...,
+		), "create_domain").ServeHTTP)
 
-	dr.Post("", otelhttp.NewHandler(kithttp.NewServer(
-		createDomainEndpoint(svc),
-		decodeCreateDomainRequest,
-		api.EncodeResponse,
-		opts...,
-	), "create_domain"))
+		r.Get("/", otelhttp.NewHandler(kithttp.NewServer(
+			listDomainsEndpoint(svc),
+			decodeListDomainRequest,
+			api.EncodeResponse,
+			opts...,
+		), "list_domains").ServeHTTP)
 
-	dr.Get("/:domainID", otelhttp.NewHandler(kithttp.NewServer(
-		retrieveDomainEndpoint(svc),
-		decodeRetrieveDomainRequest,
-		api.EncodeResponse,
-		opts...,
-	), "view_domain"))
+		r.Route("/{domainID}", func(r chi.Router) {
+			r.Get("/", otelhttp.NewHandler(kithttp.NewServer(
+				retrieveDomainEndpoint(svc),
+				decodeRetrieveDomainRequest,
+				api.EncodeResponse,
+				opts...,
+			), "view_domain").ServeHTTP)
+			r.Patch("/", otelhttp.NewHandler(kithttp.NewServer(
+				updateDomainEndpoint(svc),
+				decodeUpdateDomainRequest,
+				api.EncodeResponse,
+				opts...,
+			), "update_domain").ServeHTTP)
+			r.Post("/enable", otelhttp.NewHandler(kithttp.NewServer(
+				enableDomainEndpoint(svc),
+				decodeEnableDomainRequest,
+				api.EncodeResponse,
+				opts...,
+			), "enable_domain").ServeHTTP)
+			r.Post("/disable", otelhttp.NewHandler(kithttp.NewServer(
+				disableDomainEndpoint(svc),
+				decodeDisableDomainRequest,
+				api.EncodeResponse,
+				opts...,
+			), "disable_domain").ServeHTTP)
+			r.Route("/users", func(r chi.Router) {
+				r.Post("/assign", otelhttp.NewHandler(kithttp.NewServer(
+					assignDomainUsersEndpoint(svc),
+					decodeAssignUsersRequest,
+					api.EncodeResponse,
+					opts...,
+				), "assign_domain_users").ServeHTTP)
+				r.Post("/unassign", otelhttp.NewHandler(kithttp.NewServer(
+					unassignDomainUsersEndpoint(svc),
+					decodeUnassignUsersRequest,
+					api.EncodeResponse,
+					opts...,
+				), "unassign_domain_users").ServeHTTP)
+			})
 
-	dr.Patch("/:domainID", otelhttp.NewHandler(kithttp.NewServer(
-		updateDomainEndpoint(svc),
-		decodeUpdateDomainRequest,
-		api.EncodeResponse,
-		opts...,
-	), "update_domain"))
-
-	dr.Get("", otelhttp.NewHandler(kithttp.NewServer(
-		listDomainsEndpoint(svc),
-		decodeListDomainRequest,
-		api.EncodeResponse,
-		opts...,
-	), "list_domains"))
-
-	dr.Post("/:domainID/enable", otelhttp.NewHandler(kithttp.NewServer(
-		enableDomainEndpoint(svc),
-		decodeEnableDomainRequest,
-		api.EncodeResponse,
-		opts...,
-	), "enable_domain"))
-
-	dr.Post("/:domainID/disable", otelhttp.NewHandler(kithttp.NewServer(
-		disableDomainEndpoint(svc),
-		decodeDisableDomainRequest,
-		api.EncodeResponse,
-		opts...,
-	), "disable_domain"))
-
-	dr.Post("/:domainID/users/assign", otelhttp.NewHandler(kithttp.NewServer(
-		assignDomainUsersEndpoint(svc),
-		decodeAssignUsersRequest,
-		api.EncodeResponse,
-		opts...,
-	), "assign_domain_users"))
-
-	dr.Post("/:domainID/users/unassign", otelhttp.NewHandler(kithttp.NewServer(
-		unassignDomainUsersEndpoint(svc),
-		decodeUnassignUsersRequest,
-		api.EncodeResponse,
-		opts...,
-	), "unassign_domain_users"))
-
-	r.SubRoute("/domains", dr)
-
-	r.Get("/users/:userID/domains", otelhttp.NewHandler(kithttp.NewServer(
+		})
+	})
+	mux.Get("/users/{userID}/domains", otelhttp.NewHandler(kithttp.NewServer(
 		listUserDomainsEndpoint(svc),
 		decodeListUserDomainsRequest,
 		api.EncodeResponse,
 		opts...,
-	), "list_domains_by_user_id"))
+	), "list_domains_by_user_id").ServeHTTP)
 
-	return r
+	return mux
 }
