d07e3b70c79d5cd060193a692c12e5df8fe2e642
felix.gateru <felix.gateru@gmail.com>
2023-12-01 05:53:35 +0300

    Fix errors
    Signed-off-by: felix.gateru <felix.gateru@gmail.com>

--- internal/api/common.go
+++ internal/api/common.go
@@ -115,7 +115,8 @@ func EncodeError(_ context.Context, err error, w http.ResponseWriter) {
 		errors.Contains(err, apiutil.ErrLimitSize),
 		errors.Contains(err, apiutil.ErrNameSize):
 		w.WriteHeader(http.StatusBadRequest)
-	case errors.Contains(err, errors.ErrAuthentication),
+	case errors.Contains(err, apiutil.ErrValidation),
+		errors.Contains(err, errors.ErrAuthentication),
 		errors.Contains(err, apiutil.ErrBearerToken):
 		w.WriteHeader(http.StatusUnauthorized)
 	case errors.Contains(err, svcerr.ErrNotFound):
