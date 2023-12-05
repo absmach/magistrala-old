4ac795368a3beb852ccc75092c6e90fdf2b9dc82
Rodney Osodo <28790446+rodneyosodo@users.noreply.github.com>
2023-12-05 18:21:59 +0300

    feat(service): Add new "invitations" service
    This commit adds a new service called "invitations" to the existing file. The service includes the necessary imports and initializes components for its functionality. It also includes configuration settings and a Docker Compose file. Additionally, instructions for deploying and using the service are provided, along with a function to create an HTTP handler.
    
    Signed-off-by: Rodney Osodo <28790446+rodneyosodo@users.noreply.github.com>

---
+++ pkg/sdk/go/invitations.go
@@ -0,0 +1,109 @@
+// Copyright (c) Abstract Machines
+// SPDX-License-Identifier: Apache-2.0
+
+package sdk
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/absmach/magistrala/pkg/errors"
+)
+
+const (
+	invitationsEndpoint = "invitations"
+	acceptEndpoint      = "accept"
+)
+
+type Invitation struct {
+	InvitedBy   string    `json:"invited_by" db:"invited_by"`
+	UserID      string    `json:"user_id" db:"user_id"`
+	Domain      string    `json:"domain" db:"domain"`
+	Token       string    `json:"token,omitempty" db:"token"`
+	Relation    string    `json:"relation,omitempty" db:"relation"`
+	CreatedAt   time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at,omitempty" db:"updated_at,omitempty"`
+	ConfirmedAt time.Time `json:"confirmed_at,omitempty" db:"confirmed_at,omitempty"`
+	Resend      bool      `json:"resend,omitempty"`
+}
+
+type InvitationPage struct {
+	Total       uint64       `json:"total"`
+	Offset      uint64       `json:"offset"`
+	Limit       uint64       `json:"limit"`
+	Invitations []Invitation `json:"invitations"`
+}
+
+func (sdk mgSDK) SendInvitation(invitation Invitation, token string) (err error) {
+	data, err := json.Marshal(invitation)
+	if err != nil {
+		return errors.NewSDKError(err)
+	}
+
+	url := sdk.invitationsURL + "/" + invitationsEndpoint
+
+	_, _, sdkerr := sdk.processRequest(http.MethodPost, url, token, data, nil, http.StatusCreated)
+
+	return sdkerr
+}
+
+func (sdk mgSDK) Invitation(userID, domainID, token string) (invitation Invitation, err error) {
+	url := sdk.invitationsURL + "/" + invitationsEndpoint + "/" + userID + "/" + domainID
+
+	_, body, sdkerr := sdk.processRequest(http.MethodGet, url, token, nil, nil, http.StatusOK)
+	if sdkerr != nil {
+		return Invitation{}, sdkerr
+	}
+
+	if err := json.Unmarshal(body, &invitation); err != nil {
+		return Invitation{}, errors.NewSDKError(err)
+	}
+
+	return invitation, nil
+}
+
+func (sdk mgSDK) Invitations(pm PageMetadata, token string) (invitations InvitationPage, err error) {
+	url, err := sdk.withQueryParams(sdk.invitationsURL, invitationsEndpoint, pm)
+	if err != nil {
+		return InvitationPage{}, errors.NewSDKError(err)
+	}
+
+	_, body, sdkerr := sdk.processRequest(http.MethodGet, url, token, nil, nil, http.StatusOK)
+	if sdkerr != nil {
+		return InvitationPage{}, sdkerr
+	}
+
+	var invPage InvitationPage
+	if err := json.Unmarshal(body, &invPage); err != nil {
+		return InvitationPage{}, errors.NewSDKError(err)
+	}
+
+	return invPage, nil
+}
+
+func (sdk mgSDK) AcceptInvitation(token string) (domains []string, err error) {
+	url := sdk.invitationsURL + "/" + invitationsEndpoint + "/" + acceptEndpoint
+
+	_, body, sdkerr := sdk.processRequest(http.MethodPost, url, token, nil, nil, http.StatusOK)
+	if sdkerr != nil {
+		return nil, sdkerr
+	}
+
+	var resp struct {
+		Domains []string `json:"domains"`
+	}
+	if err := json.Unmarshal(body, &resp); err != nil {
+		return nil, errors.NewSDKError(err)
+	}
+
+	return resp.Domains, nil
+}
+
+func (sdk mgSDK) DeleteInvitation(userID, domain, token string) (err error) {
+	url := sdk.invitationsURL + "/" + invitationsEndpoint + "/" + domain + "/" + userID
+
+	_, _, sdkerr := sdk.processRequest(http.MethodDelete, url, token, nil, nil, http.StatusNoContent)
+
+	return sdkerr
+}
