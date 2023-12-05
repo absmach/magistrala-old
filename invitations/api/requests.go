c98d83bb70cf2f4f9545acc434372174fab875e6
Rodney Osodo <28790446+rodneyosodo@users.noreply.github.com>
2023-12-05 18:01:40 +0300

    feat(api): Implement endpoints for invitations
    Implemented endpoints for sending, viewing, accepting, and deleting invitations in the API. Added validation, error handling, logging, and metrics functionality. Utilized various packages, routers, and frameworks to handle HTTP requests and perform invitation operations. Also included functions for decoding request payloads and implementing tracing functionality.
    
    Signed-off-by: Rodney Osodo <28790446+rodneyosodo@users.noreply.github.com>

---
+++ invitations/api/requests.go
@@ -0,0 +1,91 @@
+// Copyright (c) Abstract Machines
+// SPDX-License-Identifier: Apache-2.0
+
+package api
+
+import (
+	"errors"
+
+	"github.com/absmach/magistrala/internal/apiutil"
+	"github.com/absmach/magistrala/invitations"
+)
+
+const maxLimitSize = 100
+
+var errMissingDomain = errors.New("missing domain")
+
+type sendInvitationReq struct {
+	token    string
+	UserID   string `json:"user_id,omitempty"`
+	Domain   string `json:"domain,omitempty"`
+	Relation string `json:"relation,omitempty"`
+	Resend   bool   `json:"resend,omitempty"`
+}
+
+func (req *sendInvitationReq) validate() error {
+	if req.token == "" {
+		return apiutil.ErrBearerToken
+	}
+	if req.UserID == "" {
+		return apiutil.ErrMissingID
+	}
+	if req.Domain == "" {
+		return errMissingDomain
+	}
+	if err := invitations.CheckRelation(req.Relation); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type listInvitationsReq struct {
+	token string
+	invitations.Page
+}
+
+func (req *listInvitationsReq) validate() error {
+	if req.token == "" {
+		return apiutil.ErrBearerToken
+	}
+	if req.Page.Limit > maxLimitSize || req.Page.Limit < 1 {
+		return apiutil.ErrLimitSize
+	}
+	if req.Page.Domain == "" {
+		return errMissingDomain
+	}
+
+	return nil
+}
+
+type acceptInvitationReq struct {
+	token string
+}
+
+func (req *acceptInvitationReq) validate() error {
+	if req.token == "" {
+		return apiutil.ErrBearerToken
+	}
+
+	return nil
+}
+
+type invitationReq struct {
+	token  string
+	userID string
+	domain string
+}
+
+func (req *invitationReq) validate() error {
+	if req.token == "" {
+		return apiutil.ErrBearerToken
+	}
+	if req.userID == "" {
+		return apiutil.ErrMissingID
+	}
+	if req.domain == "" {
+		return errMissingDomain
+	}
+
+	return nil
+}
---
+++ invitations/middleware/tracing.go
@@ -0,0 +1,75 @@
+// Copyright (c) Abstract Machines
+// SPDX-License-Identifier: Apache-2.0
+
+package middleware
+
+import (
+	"context"
+
+	"github.com/absmach/magistrala/invitations"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
+)
+
+var _ invitations.Service = (*tracing)(nil)
+
+type tracing struct {
+	tracer trace.Tracer
+	svc    invitations.Service
+}
+
+func Tracing(svc invitations.Service, tracer trace.Tracer) invitations.Service {
+	return &tracing{tracer, svc}
+}
+
+func (tm *tracing) SendInvitation(ctx context.Context, token string, invitation invitations.Invitation) (err error) {
+	ctx, span := tm.tracer.Start(ctx, "send_invitation", trace.WithAttributes(
+		attribute.String("domain", invitation.Domain),
+		attribute.String("user_id", invitation.UserID),
+	))
+	defer span.End()
+
+	return tm.svc.SendInvitation(ctx, token, invitation)
+}
+
+func (tm *tracing) ViewInvitation(ctx context.Context, token, userID, domain string) (invitation invitations.Invitation, err error) {
+	ctx, span := tm.tracer.Start(ctx, "view_invitation", trace.WithAttributes(
+		attribute.String("user_id", userID),
+		attribute.String("domain", domain),
+	))
+	defer span.End()
+
+	return tm.svc.ViewInvitation(ctx, token, userID, domain)
+}
+
+func (tm *tracing) ListInvitations(ctx context.Context, token string, page invitations.Page) (invitations invitations.InvitationPage, err error) {
+	ctx, span := tm.tracer.Start(ctx, "list_invitations", trace.WithAttributes(
+		attribute.Int("limit", int(page.Limit)),
+		attribute.Int("offset", int(page.Offset)),
+		attribute.String("user_id", page.UserID),
+		attribute.String("domain", page.Domain),
+		attribute.String("invited_by", page.InvitedBy),
+	))
+	defer span.End()
+
+	return tm.svc.ListInvitations(ctx, token, page)
+}
+
+func (tm *tracing) AcceptInvitation(ctx context.Context, token string) (domains []string, err error) {
+	ctx, span := tm.tracer.Start(ctx, "accept_invitation", trace.WithAttributes(
+		attribute.StringSlice("domains", domains),
+	))
+	defer span.End()
+
+	return tm.svc.AcceptInvitation(ctx, token)
+}
+
+func (tm *tracing) DeleteInvitation(ctx context.Context, token string, userID, domain string) (err error) {
+	ctx, span := tm.tracer.Start(ctx, "delete_invitation", trace.WithAttributes(
+		attribute.String("user_id", userID),
+		attribute.String("domain", domain),
+	))
+	defer span.End()
+
+	return tm.svc.DeleteInvitation(ctx, token, userID, domain)
+}
