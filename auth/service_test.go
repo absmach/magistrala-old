2e386111cd4bc32e95a56d9e6a2656fdd5aed608
felix.gateru <felix.gateru@gmail.com>
2023-12-13 15:18:17 +0300

    Fix service tests
    Signed-off-by: felix.gateru <felix.gateru@gmail.com>

--- auth/service_test.go
+++ auth/service_test.go
@@ -301,12 +301,6 @@ func TestIdentify(t *testing.T) {
 			idt:  "",
 			err:  errors.ErrAuthentication,
 		},
-		// {
-		// 	desc: "identify invalid key",
-		// 	key:  "invalid",
-		// 	idt:  "",
-		// 	err:  errors.ErrAuthentication,
-		// },
 	}
 
 	for _, tc := range cases {
