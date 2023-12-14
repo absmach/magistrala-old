ff5d3ce95b03d0f90afa661d9ee36323bf12ef70
felix.gateru <felix.gateru@gmail.com>
2023-12-14 16:59:53 +0300

    Fix auth errors
    Signed-off-by: felix.gateru <felix.gateru@gmail.com>

--- auth/service.go
+++ auth/service.go
@@ -190,8 +190,6 @@ func (svc service) Authorize(ctx context.Context, pr PolicyReq) error {
 	if err := svc.agent.CheckPolicy(ctx, pr); err != nil {
 		return errors.Wrap(svcerr.ErrAuthorization, err)
 	}
-	fmt.Println("Got here")
-	defer fmt.Println("returned nil")
 	return nil
 }
 
