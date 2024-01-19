17347bdf36651f310614df81e5cb400c21deadb5
felix.gateru <felix.gateru@gmail.com>
2024-01-19 16:54:52 +0300

    Increase coverage
    Signed-off-by: felix.gateru <felix.gateru@gmail.com>

--- auth/domains_test.go
+++ auth/domains_test.go
@@ -4,7 +4,6 @@
 package auth_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/absmach/magistrala/auth"
@@ -12,67 +11,165 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestString(t *testing.T) {
-	assert.Equal(t, auth.DisabledStatus.String(), "disabled")
-	assert.Equal(t, auth.EnabledStatus.String(), "enabled")
-	assert.Equal(t, auth.FreezeStatus.String(), "freezed")
-	assert.Equal(t, auth.AllStatus.String(), "all")
-	assert.Equal(t, auth.Status(5).String(), "unknown")
+func TestStatusString(t *testing.T) {
+	cases := []struct {
+		desc     string
+		status   auth.Status
+		expected string
+	}{
+		{
+			desc:     "Enabled",
+			status:   auth.EnabledStatus,
+			expected: "enabled",
+		},
+		{
+			desc:     "Disabled",
+			status:   auth.DisabledStatus,
+			expected: "disabled",
+		},
+		{
+			desc:     "All",
+			status:   auth.AllStatus,
+			expected: "all",
+		},
+		{
+			desc:     "Unknown",
+			status:   auth.Status(100),
+			expected: "unknown",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.desc, func(t *testing.T) {
+			got := tc.status.String()
+			assert.Equal(t, tc.expected, got, "String() = %v, expected %v", got, tc.expected)
+		})
+	}
 }
 
 func TestToStatus(t *testing.T) {
 	cases := []struct {
-		status   string
-		expected auth.Status
-		err      error
+		desc      string
+		status    string
+		expetcted auth.Status
+		err       error
 	}{
-		{"disabled", auth.DisabledStatus, nil},
-		{"enabled", auth.EnabledStatus, nil},
-		{"freezed", auth.FreezeStatus, nil},
-		{"all", auth.AllStatus, nil},
-		{"unknown", auth.EnabledStatus, apiutil.ErrInvalidStatus},
+		{
+			desc:      "Enabled",
+			status:    "enabled",
+			expetcted: auth.EnabledStatus,
+			err:       nil,
+		},
+		{
+			desc:      "Disabled",
+			status:    "disabled",
+			expetcted: auth.DisabledStatus,
+			err:       nil,
+		},
+		{
+			desc:      "All",
+			status:    "all",
+			expetcted: auth.AllStatus,
+			err:       nil,
+		},
+		{
+			desc:      "Unknown",
+			status:    "unknown",
+			expetcted: auth.Status(0),
+			err:       apiutil.ErrInvalidStatus,
+		},
 	}
-	for _, c := range cases {
-		status, err := auth.ToStatus(c.status)
-		assert.Equal(t, status, c.expected, fmt.Sprintf("To %s failed with status: %v", c.status, status))
-		assert.Equal(t, c.err, err, fmt.Sprintf("To %s failed with error: %v", c.status, err))
+
+	for _, tc := range cases {
+		t.Run(tc.desc, func(t *testing.T) {
+			got, err := auth.ToStatus(tc.status)
+			assert.Equal(t, tc.err, err, "ToStatus() error = %v, expected %v", err, tc.err)
+			assert.Equal(t, tc.expetcted, got, "ToStatus() = %v, expected %v", got, tc.expetcted)
+		})
 	}
 }
 
-func TestMarshalJSON(t *testing.T) {
+func TestStatusMarshalJSON(t *testing.T) {
 	cases := []struct {
+		desc     string
+		expected []byte
 		status   auth.Status
-		expected string
+		err      error
 	}{
-		{auth.DisabledStatus, `"disabled"`},
-		{auth.EnabledStatus, `"enabled"`},
-		{auth.FreezeStatus, `"freezed"`},
-		{auth.AllStatus, `"all"`},
-		{auth.Status(5), `"unknown"`},
+		{
+			desc:     "Enabled",
+			expected: []byte(`"enabled"`),
+			status:   auth.EnabledStatus,
+			err:      nil,
+		},
+		{
+			desc:     "Disabled",
+			expected: []byte(`"disabled"`),
+			status:   auth.DisabledStatus,
+			err:      nil,
+		},
+		{
+			desc:     "All",
+			expected: []byte(`"all"`),
+			status:   auth.AllStatus,
+			err:      nil,
+		},
+		{
+			desc:     "Unknown",
+			expected: []byte(`"unknown"`),
+			status:   auth.Status(100),
+			err:      nil,
+		},
 	}
-	for _, c := range cases {
-		b, err := c.status.MarshalJSON()
-		assert.Equal(t, c.expected, string(b), fmt.Sprintf("MarshalJSON failed with status: %v", c.status))
-		assert.Nil(t, err, fmt.Sprintf("MarshalJSON failed with error: %v", err))
+
+	for _, tc := range cases {
+		t.Run(tc.desc, func(t *testing.T) {
+			got, err := tc.status.MarshalJSON()
+			assert.Equal(t, tc.err, err, "MarshalJSON() error = %v, expected %v", err, tc.err)
+			assert.Equal(t, tc.expected, got, "MarshalJSON() = %v, expected %v", got, tc.expected)
+		})
 	}
 }
 
-func TestUnmarshalJSON(t *testing.T) {
+func TestStatusUnmarshalJSON(t *testing.T) {
 	cases := []struct {
-		status   string
+		desc     string
 		expected auth.Status
+		status   []byte
 		err      error
 	}{
-		{`"disabled"`, auth.DisabledStatus, nil},
-		{`"enabled"`, auth.EnabledStatus, nil},
-		{`"freezed"`, auth.FreezeStatus, nil},
-		{`"all"`, auth.AllStatus, nil},
-		{`"unknown"`, auth.EnabledStatus, apiutil.ErrInvalidStatus},
+		{
+			desc:     "Enabled",
+			expected: auth.EnabledStatus,
+			status:   []byte(`"enabled"`),
+			err:      nil,
+		},
+		{
+			desc:     "Disabled",
+			expected: auth.DisabledStatus,
+			status:   []byte(`"disabled"`),
+			err:      nil,
+		},
+		{
+			desc:     "All",
+			expected: auth.AllStatus,
+			status:   []byte(`"all"`),
+			err:      nil,
+		},
+		{
+			desc:     "Unknown",
+			expected: auth.Status(0),
+			status:   []byte(`"unknown"`),
+			err:      apiutil.ErrInvalidStatus,
+		},
 	}
-	for _, c := range cases {
-		var status auth.Status
-		err := status.UnmarshalJSON([]byte(c.status))
-		assert.Equal(t, c.expected, status, fmt.Sprintf("UnmarshalJSON failed with status: %v", status))
-		assert.Equal(t, c.err, err, fmt.Sprintf("UnmarshalJSON failed with error: %v", err))
+
+	for _, tc := range cases {
+		t.Run(tc.desc, func(t *testing.T) {
+			var s auth.Status
+			err := s.UnmarshalJSON(tc.status)
+			assert.Equal(t, tc.err, err, "UnmarshalJSON() error = %v, expected %v", err, tc.err)
+			assert.Equal(t, tc.expected, s, "UnmarshalJSON() = %v, expected %v", s, tc.expected)
+		})
 	}
 }
