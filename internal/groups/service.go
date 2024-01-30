absmach/magistrala

Add property based testing to users API using schemathesis
Signed-off-by: Rodney Osodo <28790446+rodneyosodo@users.noreply.github.com>

internal/groups/service.go
@@ -13,6 +13,8 @@ import (
 	"github.com/absmach/magistrala/internal/apiutil"
 	mgclients "github.com/absmach/magistrala/pkg/clients"
 	"github.com/absmach/magistrala/pkg/errors"
+	repoerr "github.com/absmach/magistrala/pkg/errors/repository"
+	svcerr "github.com/absmach/magistrala/pkg/errors/service"
 	"github.com/absmach/magistrala/pkg/groups"
 	"golang.org/x/sync/errgroup"
 )
@@ -20,8 +22,6 @@ import (
 var (
 	errParentUnAuthz  = errors.New("failed to authorize parent group")
 	errMemberKind     = errors.New("invalid member kind")
-	errAddPolicies    = errors.New("failed to add policies")
-	errDeletePolicies = errors.New("failed to delete policies")
 	errRetrieveGroups = errors.New("failed to retrieve groups")
 	errGroupIDs       = errors.New("invalid group ids")
 )
@@ -284,10 +284,10 @@ func (svc service) checkSuperAdmin(ctx context.Context, userID string) error {
 		Object:      auth.MagistralaObject,
 	})
 	if err != nil {
-		return err
+		return errors.Wrap(svcerr.ErrAuthorization, err)
 	}
 	if !res.Authorized {
-		return errors.ErrAuthorization
+		return svcerr.ErrAuthorization
 	}
 	return nil
 }
@@ -445,7 +445,7 @@ func (svc service) Assign(ctx context.Context, token, groupID, relation, memberK
 	}
 
 	if _, err := svc.auth.AddPolicies(ctx, &policies); err != nil {
-		return errors.Wrap(errAddPolicies, err)
+		return errors.Wrap(repoerr.ErrAddPolicies, err)
 	}
 
 	return nil
@@ -596,7 +596,7 @@ func (svc service) Unassign(ctx context.Context, token, groupID, relation, membe
 	}
 
 	if _, err := svc.auth.DeletePolicies(ctx, &policies); err != nil {
-		return errors.Wrap(errDeletePolicies, err)
+		return errors.Wrap(repoerr.ErrDeletePolicies, err)
 	}
 	return nil
 }
@@ -641,7 +641,7 @@ func (svc service) changeGroupStatus(ctx context.Context, token string, group gr
 		return groups.Group{}, err
 	}
 	if dbGroup.Status == group.Status {
-		return groups.Group{}, mgclients.ErrStatusAlreadyAssigned
+		return groups.Group{}, errors.ErrStatusAlreadyAssigned
 	}
 
 	group.UpdatedBy = id
@@ -670,10 +670,10 @@ func (svc service) authorize(ctx context.Context, subjectType, subject, permissi
 	}
 	res, err := svc.auth.Authorize(ctx, req)
 	if err != nil {
-		return "", err
+		return "", errors.Wrap(svcerr.ErrAuthorization, err)
 	}
 	if !res.GetAuthorized() {
-		return "", errors.ErrAuthorization
+		return "", svcerr.ErrAuthorization
 	}
 	return res.GetId(), nil
 }
@@ -689,10 +689,10 @@ func (svc service) authorizeKind(ctx context.Context, subjectType, subjectKind, 
 	}
 	res, err := svc.auth.Authorize(ctx, req)
 	if err != nil {
-		return "", err
+		return "", errors.Wrap(svcerr.ErrAuthorization, err)
 	}
 	if !res.GetAuthorized() {
-		return "", errors.ErrAuthorization
+		return "", svcerr.ErrAuthorization
 	}
 	return res.GetId(), nil
 }

pkg/sdk/go/users_test.go
@@ -86,7 +86,7 @@ func TestCreateClient(t *testing.T) {
 			client:   user,
 			response: sdk.User{},
 			token:    token,
-			err:      errors.NewSDKErrorWithStatus(errors.Wrap(apiutil.ErrValidation, sdk.ErrFailedCreation), http.StatusInternalServerError),
+			err:      errors.NewSDKErrorWithStatus(errors.Wrap(apiutil.ErrValidation, sdk.ErrFailedCreation), http.StatusUnprocessableEntity),
 		},
 		{
 			desc:     "register empty user",
@@ -367,7 +367,6 @@ func TestListClients(t *testing.T) {
 	for _, tc := range cases {
 		pm := sdk.PageMetadata{
 			Status:   tc.status,
-			Total:    total,
 			Offset:   tc.offset,
 			Limit:    tc.limit,
 			Name:     tc.name,
@@ -920,7 +919,7 @@ func TestUpdateClientRole(t *testing.T) {
 			client:   client2,
 			response: sdk.User{},
 			token:    validToken,
-			err:      errors.NewSDKErrorWithStatus(errors.Wrap(users.ErrFailedOwnerUpdate, users.ErrFailedOwnerUpdate), http.StatusInternalServerError),
+			err:      errors.NewSDKErrorWithStatus(errors.Wrap(svcerr.ErrFailedOwnerUpdate, svcerr.ErrFailedOwnerUpdate), http.StatusInternalServerError),
 		},
 		{
 			desc: "update a user that can't be marshalled",

users/service.go
@@ -18,17 +18,6 @@ import (
 )
 
 var (
-	// ErrRecoveryToken indicates error in generating password recovery token.
-	ErrRecoveryToken = errors.New("failed to generate password recovery token")
-
-	// ErrPasswordFormat indicates weak password.
-	ErrPasswordFormat = errors.New("password does not meet the requirements")
-
-	// ErrFailedPolicyUpdate indicates a failure to update user policy.
-	ErrFailedPolicyUpdate = errors.New("failed to update user policy")
-
-	// ErrFailedOwnerUpdate indicates a failure to update user policy.
-	ErrFailedOwnerUpdate = errors.New("failed to update user owner")
 
 	// ErrAddPolicies indictaed a failre to add policies.
 	errAddPolicies = errors.New("failed to add policies")
@@ -268,7 +257,7 @@ func (svc service) GenerateResetToken(ctx context.Context, email, host string) e
 	}
 	token, err := svc.auth.Issue(ctx, issueReq)
 	if err != nil {
-		return errors.Wrap(ErrRecoveryToken, err)
+		return errors.Wrap(svcerr.ErrRecoveryToken, err)
 	}
 
 	return svc.SendPasswordReset(ctx, host, email, client.Name, token.AccessToken)
@@ -287,7 +276,7 @@ func (svc service) ResetSecret(ctx context.Context, resetToken, secret string) e
 		return errors.ErrNotFound
 	}
 	if !svc.passRegex.MatchString(secret) {
-		return ErrPasswordFormat
+		return errors.ErrPasswordFormat
 	}
 	secret, err = svc.hasher.Hash(secret)
 	if err != nil {
@@ -313,7 +302,7 @@ func (svc service) UpdateClientSecret(ctx context.Context, token, oldSecret, new
 		return mgclients.Client{}, err
 	}
 	if !svc.passRegex.MatchString(newSecret) {
-		return mgclients.Client{}, ErrPasswordFormat
+		return mgclients.Client{}, errors.ErrPasswordFormat
 	}
 	dbClient, err := svc.clients.RetrieveByID(ctx, id)
 	if err != nil {
@@ -355,7 +344,7 @@ func (svc service) UpdateClientRole(ctx context.Context, token string, cli mgcli
 	}
 
 	if err := svc.updateClientPolicy(ctx, cli.ID, cli.Role); err != nil {
-		return mgclients.Client{}, errors.Wrap(ErrFailedPolicyUpdate, err)
+		return mgclients.Client{}, errors.Wrap(svcerr.ErrFailedPolicyUpdate, err)
 	}
 	client, err = svc.clients.UpdateRole(ctx, client)
 	if err != nil {
@@ -363,7 +352,7 @@ func (svc service) UpdateClientRole(ctx context.Context, token string, cli mgcli
 		if errRollback := svc.updateClientPolicy(ctx, cli.ID, mgclients.UserRole); errRollback != nil {
 			return mgclients.Client{}, errors.Wrap(err, errors.Wrap(repoerr.ErrRollbackTx, errRollback))
 		}
-		return mgclients.Client{}, errors.Wrap(ErrFailedOwnerUpdate, err)
+		return mgclients.Client{}, errors.Wrap(svcerr.ErrFailedOwnerUpdate, err)
 	}
 	return client, nil
 }
@@ -409,7 +398,7 @@ func (svc service) changeClientStatus(ctx context.Context, token string, client 
 		return mgclients.Client{}, errors.Wrap(repoerr.ErrNotFound, err)
 	}
 	if dbClient.Status == client.Status {
-		return mgclients.Client{}, mgclients.ErrStatusAlreadyAssigned
+		return mgclients.Client{}, errors.ErrStatusAlreadyAssigned
 	}
 	client.UpdatedBy = tokenUserID
 	return svc.clients.ChangeStatus(ctx, client)
@@ -495,7 +484,7 @@ func (svc *service) authorize(ctx context.Context, subjType, subjKind, subj, per
 	}
 
 	if !res.GetAuthorized() {
-		return "", errors.Wrap(svcerr.ErrAuthorization, err)
+		return "", svcerr.ErrAuthorization
 	}
 	return res.GetId(), nil
 }

absmach/magistrala-old

internal/groups/service.go
@@ -13,6 +13,7 @@ import (
 	"github.com/absmach/magistrala/internal/apiutil"
 	mgclients "github.com/absmach/magistrala/pkg/clients"
 	"github.com/absmach/magistrala/pkg/errors"
+	svcerr "github.com/absmach/magistrala/pkg/errors/service"
 	"github.com/absmach/magistrala/pkg/groups"
 	"golang.org/x/sync/errgroup"
 )
@@ -20,8 +21,6 @@ import (
 var (
 	errParentUnAuthz  = errors.New("failed to authorize parent group")
 	errMemberKind     = errors.New("invalid member kind")
-	errAddPolicies    = errors.New("failed to add policies")
-	errDeletePolicies = errors.New("failed to delete policies")
 	errRetrieveGroups = errors.New("failed to retrieve groups")
 	errGroupIDs       = errors.New("invalid group ids")
 )
@@ -284,10 +283,10 @@ func (svc service) checkSuperAdmin(ctx context.Context, userID string) error {
 		Object:      auth.MagistralaObject,
 	})
 	if err != nil {
-		return err
+		return errors.Wrap(svcerr.ErrAuthorization, err)
 	}
 	if !res.Authorized {
-		return errors.ErrAuthorization
+		return svcerr.ErrAuthorization
 	}
 	return nil
 }
@@ -445,7 +444,7 @@ func (svc service) Assign(ctx context.Context, token, groupID, relation, memberK
 	}
 
 	if _, err := svc.auth.AddPolicies(ctx, &policies); err != nil {
-		return errors.Wrap(errAddPolicies, err)
+		return errors.Wrap(svcerr.ErrAddPolicies, err)
 	}
 
 	return nil
@@ -596,7 +595,7 @@ func (svc service) Unassign(ctx context.Context, token, groupID, relation, membe
 	}
 
 	if _, err := svc.auth.DeletePolicies(ctx, &policies); err != nil {
-		return errors.Wrap(errDeletePolicies, err)
+		return errors.Wrap(svcerr.ErrDeletePolicies, err)
 	}
 	return nil
 }
@@ -694,7 +693,7 @@ func (svc service) changeGroupStatus(ctx context.Context, token string, group gr
 		return groups.Group{}, err
 	}
 	if dbGroup.Status == group.Status {
-		return groups.Group{}, mgclients.ErrStatusAlreadyAssigned
+		return groups.Group{}, errors.ErrStatusAlreadyAssigned
 	}
 
 	group.UpdatedBy = id
@@ -723,10 +722,10 @@ func (svc service) authorizeToken(ctx context.Context, subjectType, subject, per
 	}
 	res, err := svc.auth.Authorize(ctx, req)
 	if err != nil {
-		return "", err
+		return "", errors.Wrap(svcerr.ErrAuthorization, err)
 	}
 	if !res.GetAuthorized() {
-		return "", errors.ErrAuthorization
+		return "", svcerr.ErrAuthorization
 	}
 	return res.GetId(), nil
 }
@@ -743,10 +742,10 @@ func (svc service) authorizeKind(ctx context.Context, domainID, subjectType, sub
 	}
 	res, err := svc.auth.Authorize(ctx, req)
 	if err != nil {
-		return "", err
+		return "", errors.Wrap(svcerr.ErrAuthorization, err)
 	}
 	if !res.GetAuthorized() {
-		return "", errors.ErrAuthorization
+		return "", svcerr.ErrAuthorization
 	}
 	return res.GetId(), nil
 }

pkg/sdk/go/users_test.go
@@ -86,7 +86,7 @@ func TestCreateClient(t *testing.T) {
 			client:   user,
 			response: sdk.User{},
 			token:    token,
-			err:      errors.NewSDKErrorWithStatus(errors.Wrap(sdk.ErrFailedCreation, sdk.ErrFailedCreation), http.StatusInternalServerError),
+			err:      errors.NewSDKErrorWithStatus(errors.Wrap(sdk.ErrFailedCreation, sdk.ErrFailedCreation), http.StatusUnprocessableEntity),
 		},
 		{
 			desc:     "register empty user",
@@ -354,7 +354,6 @@ func TestListClients(t *testing.T) {
 	for _, tc := range cases {
 		pm := sdk.PageMetadata{
 			Status:   tc.status,
-			Total:    total,
 			Offset:   tc.offset,
 			Limit:    tc.limit,
 			Name:     tc.name,
@@ -556,7 +555,7 @@ func TestUpdateClient(t *testing.T) {
 			client:   client2,
 			response: sdk.User{},
 			token:    validToken,
-			err:      errors.NewSDKErrorWithStatus(errors.Wrap(svcerr.ErrUpdateEntity, svcerr.ErrUpdateEntity), http.StatusInternalServerError),
+			err:      errors.NewSDKErrorWithStatus(errors.Wrap(svcerr.ErrUpdateEntity, svcerr.ErrUpdateEntity), http.StatusUnprocessableEntity),
 		},
 		{
 			desc: "update a user that can't be marshalled",
@@ -647,7 +646,7 @@ func TestUpdateClientTags(t *testing.T) {
 			client:   client2,
 			response: sdk.User{},
 			token:    validToken,
-			err:      errors.NewSDKErrorWithStatus(errors.Wrap(svcerr.ErrUpdateEntity, svcerr.ErrUpdateEntity), http.StatusInternalServerError),
+			err:      errors.NewSDKErrorWithStatus(errors.Wrap(svcerr.ErrUpdateEntity, svcerr.ErrUpdateEntity), http.StatusUnprocessableEntity),
 		},
 		{
 			desc: "update a user that can't be marshalled",
@@ -737,7 +736,7 @@ func TestUpdateClientIdentity(t *testing.T) {
 			client:   client2,
 			response: sdk.User{},
 			token:    validToken,
-			err:      errors.NewSDKErrorWithStatus(errors.Wrap(svcerr.ErrUpdateEntity, svcerr.ErrUpdateEntity), http.StatusInternalServerError),
+			err:      errors.NewSDKErrorWithStatus(errors.Wrap(svcerr.ErrUpdateEntity, svcerr.ErrUpdateEntity), http.StatusUnprocessableEntity),
 		},
 		{
 			desc: "update a user that can't be marshalled",
@@ -753,7 +752,7 @@ func TestUpdateClientIdentity(t *testing.T) {
 			},
 			response: sdk.User{},
 			token:    validToken,
-			err:      errors.NewSDKErrorWithStatus(errors.Wrap(svcerr.ErrUpdateEntity, svcerr.ErrUpdateEntity), http.StatusInternalServerError),
+			err:      errors.NewSDKErrorWithStatus(errors.Wrap(svcerr.ErrUpdateEntity, svcerr.ErrUpdateEntity), http.StatusUnprocessableEntity),
 		},
 	}
 
@@ -905,7 +904,7 @@ func TestUpdateClientRole(t *testing.T) {
 			client:   client2,
 			response: sdk.User{},
 			token:    validToken,
-			err:      errors.NewSDKErrorWithStatus(errors.Wrap(users.ErrFailedUpdateRole, users.ErrFailedUpdateRole), http.StatusInternalServerError),
+			err:      errors.NewSDKErrorWithStatus(errors.Wrap(svcerr.ErrFailedUpdateRole, svcerr.ErrFailedUpdateRole), http.StatusUnprocessableEntity),
 		},
 		{
 			desc: "update a user that can't be marshalled",

users/service.go
@@ -19,18 +19,6 @@ import (
 )
 
 var (
-	// ErrRecoveryToken indicates error in generating password recovery token.
-	ErrRecoveryToken = errors.New("failed to generate password recovery token")
-
-	// ErrPasswordFormat indicates weak password.
-	ErrPasswordFormat = errors.New("password does not meet the requirements")
-
-	// ErrFailedPolicyUpdate indicates a failure to update user policy.
-	ErrFailedPolicyUpdate = errors.New("failed to update user policy")
-
-	// ErrFailedUpdateRole indicates a failure to update user role.
-	ErrFailedUpdateRole = errors.New("failed to update user role")
-
 	// ErrAddPolicies indictaed a failre to add policies.
 	errAddPolicies = errors.New("failed to add policies")
 
@@ -292,7 +280,7 @@ func (svc service) GenerateResetToken(ctx context.Context, email, host string) e
 	}
 	token, err := svc.auth.Issue(ctx, issueReq)
 	if err != nil {
-		return errors.Wrap(ErrRecoveryToken, err)
+		return errors.Wrap(svcerr.ErrRecoveryToken, err)
 	}
 
 	return svc.SendPasswordReset(ctx, host, email, client.Name, token.AccessToken)
@@ -311,7 +299,7 @@ func (svc service) ResetSecret(ctx context.Context, resetToken, secret string) e
 		return errors.ErrNotFound
 	}
 	if !svc.passRegex.MatchString(secret) {
-		return ErrPasswordFormat
+		return errors.ErrPasswordFormat
 	}
 	secret, err = svc.hasher.Hash(secret)
 	if err != nil {
@@ -337,7 +325,7 @@ func (svc service) UpdateClientSecret(ctx context.Context, token, oldSecret, new
 		return mgclients.Client{}, err
 	}
 	if !svc.passRegex.MatchString(newSecret) {
-		return mgclients.Client{}, ErrPasswordFormat
+		return mgclients.Client{}, errors.ErrPasswordFormat
 	}
 	dbClient, err := svc.clients.RetrieveByID(ctx, id)
 	if err != nil {
@@ -384,7 +372,7 @@ func (svc service) UpdateClientRole(ctx context.Context, token string, cli mgcli
 	}
 
 	if err := svc.updateClientPolicy(ctx, cli.ID, cli.Role); err != nil {
-		return mgclients.Client{}, errors.Wrap(ErrFailedPolicyUpdate, err)
+		return mgclients.Client{}, errors.Wrap(svcerr.ErrFailedPolicyUpdate, err)
 	}
 	client, err = svc.clients.UpdateRole(ctx, client)
 	if err != nil {
@@ -392,7 +380,7 @@ func (svc service) UpdateClientRole(ctx context.Context, token string, cli mgcli
 		if errRollback := svc.updateClientPolicy(ctx, cli.ID, mgclients.UserRole); errRollback != nil {
 			return mgclients.Client{}, errors.Wrap(err, errors.Wrap(repoerr.ErrRollbackTx, errRollback))
 		}
-		return mgclients.Client{}, errors.Wrap(ErrFailedUpdateRole, err)
+		return mgclients.Client{}, errors.Wrap(svcerr.ErrFailedUpdateRole, err)
 	}
 	return client, nil
 }
@@ -438,7 +426,7 @@ func (svc service) changeClientStatus(ctx context.Context, token string, client 
 		return mgclients.Client{}, errors.Wrap(repoerr.ErrNotFound, err)
 	}
 	if dbClient.Status == client.Status {
-		return mgclients.Client{}, mgclients.ErrStatusAlreadyAssigned
+		return mgclients.Client{}, errors.ErrStatusAlreadyAssigned
 	}
 	client.UpdatedBy = tokenUserID
 
@@ -580,7 +568,7 @@ func (svc *service) authorize(ctx context.Context, subjType, subjKind, subj, per
 	}
 
 	if !res.GetAuthorized() {
-		return "", errors.Wrap(svcerr.ErrAuthorization, err)
+		return "", svcerr.ErrAuthorization
 	}
 	return res.GetId(), nil
 }